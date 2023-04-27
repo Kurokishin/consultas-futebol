50d8baa1feb67150e6faef0571d6502e69a2ac75
LuanClaiver <luanclaivermpp@gmail.com>
2023-04-27 12:04:48 -0300

    Comentarios

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import JogosData from "./components/JogosData";
 import React, { useState } from 'react';
 import axios from 'axios';
 
-// Este código criará uma rota para os componentes "HorarioInfo, JogadorID e JogosData" quando a 
-// URL /horario/:horarioId for acessada. O parâmetro :horarioId é um parâmetro de
-// rota que pode ser utilizado para buscar informações do horário selecionado na API.
-// O mesmo ocorre com as URLs dos outros parametros informados (JogadorID e JogosData).
+// Este código criará uma rota para os componentes "HorarioInfo, JogadorID e JogosData"
 
 function App() {
   // Usando o 'HorarioInfo' importado, definimos o 'horarioSelecionado' com valor 'null' usando o 'UseState'.
@@ -15,22 +12,28 @@ function App() {
   const [jogadorSelecionado, setJogadorSelecionado] = useState(null);
   const [jogos, setJogos] = useState([]);
 
-  const horario = { // Definimos um objeto 'horario' com algumas informações de exemplo para testar.
-    nome: 'Horário 1',
-    esporte: 'Futebol',
-    local: 'Campo de Futebol',
-    horarioInicio: '14:00',
-    horarioTermino: '16:00',
+  // Definimos um objeto 'horario' com algumas informações de exemplo para testar.
+  const getHorario = async () => {
+    try {
+      const response = await axios.get('https://www.api-football.com/horario');
+      setHorario(response.data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  const jogador = { // Definimos um objeto 'jogador' com algumas informações.
-    nome: 'João',
-    idade: 25,
-    posicao: 'Atacante',
-    time: 'Time A',
+  // Definimos um objeto 'jogador' com algumas informações.
+  const getJogador = async () => {
+    try {
+      const response = await axios.get('https://www.api-football.com/jogador');
+      setJogador(response.data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  const getJogos = async () => {
+  // Definimos o objeto getJogos para buscar diretamente da api via HTTP.
+  const getJogos = async () => { 
     try {
       const response = await axios.get('https://www.api-football.com/');
       setJogos(response.data);
@@ -39,23 +42,15 @@ function App() {
     }
   };
 
-  // SelecionarHorario irá atualizar o estado 'horarioSelecionado' para o valor do objeto horario.
-  const selecionarHorario = () => {
-    setHorarioSelecionado(horario);
-  };
-
-  const selecionarJogador = () => {
-    setJogadorSelecionado(jogador);
-  };
-
-  // Aqui criamos um botão para chamar a função 'selecionarHorario' e atualizar quando ele não for nulo.
+  // Aqui criamos um botão para chamar as funções de selecionar horario e jogador e o busca jogos
+  // Ao clicar no botão eles devem ser atualizados, caso não sejam nulos.
   return (
     <div>
       <h1>Minha aplicação de estatísticas esportivas</h1>
-      <button onClick={selecionarHorario}>Selecionar horário</button>
+      <button onClick={getHorario}>Selecionar horário</button>
       {horarioSelecionado && <HorarioInfo horario={horarioSelecionado} />}
       <br />
-      <button onClick={selecionarJogador}>Selecionar jogador</button>
+      <button onClick={getJogador}>Selecionar jogador</button>
       {jogadorSelecionado && <JogadorInfo jogador={jogadorSelecionado} />}
       <br />
       <button onClick={getJogos}>Buscar jogos</button>
