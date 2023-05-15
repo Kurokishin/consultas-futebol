72d6215106218bc8727213337684bb02929e4a21
Kurokishin <rafaelpereirasouza15@gmail.com>
2023-05-15 01:09:06 -0300

    fix: remove frase 'Lista de jogadores' que era sempre renderizada no html

--- src/components/jogadorInfo.js
+++ src/components/jogadorInfo.js
@@ -25,14 +25,18 @@ const GetPlayerInfo = ({ searchQuery }) => {
       }
     };
 
-    fetchData();
+    if (searchQuery) {
+      fetchData();
+    } else {
+      setPlayers([]);
+    }
   }, [searchQuery]);
 
   return (
     <div>
-      <h1>Lista de jogadores</h1>
       {players.map(player => (
         <div key={player.nome}>
+          <h1>Jogador</h1>
           <p className='nomeJogador'>Nome: {player.nome}</p>
           <p>Time: {player.time}</p>
           {player.foto && <img src={player.foto} alt={player.nome} />}
@@ -46,97 +50,3 @@ const GetPlayerInfo = ({ searchQuery }) => {
 };
 
 export default GetPlayerInfo;
-
-// const GetPlayerInfo = () => {
-//   const [players, setPlayers] = useState([]);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const jogador = 'Cristiano Ronaldo'; // Replace with the player name you want to search
-//       const apiKey = process.env.REACT_APP_API_KEY; // Replace with your actual API key
-//       const apiUrl = `https://apiv3.apifootball.com/?action=get_players&player_name=${jogador}&APIkey=${apiKey}`;
-
-//       try {
-//         const response = await axios.get(apiUrl);
-//         const filteredPlayers = response.data.map(player => ({
-//           name: player.player_name,
-//           team: player.team_name
-//         }));
-//         setPlayers(filteredPlayers);
-//       } catch (error) {
-//         console.error('Error fetching player data:', error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Player List</h1>
-//       {players.map(player => (
-//         <div key={player.name}>
-//           <p>Name: {player.name}</p>
-//           <p>Team: {player.team}</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default GetPlayerInfo;
-
-// const api = axios.create({
-//   baseURL: `https://apiv3.apifootball.com/?APIkey=${apiKey}`
-//   // headers: {
-//   //   'x-rapidapi-host': 'v3.football.api-sports.io',
-//   //   'x-rapidapi-key': apiKey,
-//     //'Content-Type': 'application/json'
-//   //}
-// });
-
-//export async function getPlayerInfo(nomeJogador) {
-// export async function getPlayerInfo(jogador) {
-//   // Fazer uma solicitação GET para a APIFootball para obter informações dos jogadores
-//   axios.get(`https://apiv3.apifootball.com/?action=get_players&player_name=${jogador}&APIkey=${apiKey}`)
-//   .then(response => {
-//       // Verificar se a resposta tem pelo menos um jogador
-//       if (response.data.length > 0) {
-//           // console.log(response.data) // Retorna todos os dados do Jogador no formato padrão da API
-//           // Navega em todos os resultados retornados pela API
-//           response.data.forEach(jogador => {
-//               // Grava em um JSON as informações desejadas do jogador retornado
-//               const resposta = {
-//                   nome: jogador.player_name, // String
-//                   clube: jogador.team_name, // String
-//                   posicao: jogador.player_type, // Forwards = Atacante || Midfielders = Meio-campista || Defenders = Defesa
-//                   urlImagem: jogador.player_image, // 150px X 150px
-//                   nacionalidade: jogador.player_country,
-//                   jogosGanhos: jogador.player_duels_won,
-//                   jogosTotais: jogador.player_duels_total,
-//                   golsMarcados: jogador.player_goals,
-//                   cartoesAmarelos: jogador.player_red_cards,
-//                   cartoesVermelhos: jogador.player_yellow_cards
-//                   //idade: jogador.player_age, // XX - int
-//                   //nascimento: jogador.player_birthdate, // YYYY-MM-DD
-//                   //numero: jogador.player_number, // XX - int
-//               }
-
-//               jsonJogadores.push(resposta);
-//           });
-//           // Retorna o array de JSONs
-//           console.log(jsonJogadores)
-//           return(jsonJogadores)
-//       } else {
-//           // Retorna se não for achado nenhum jogador com nome desejado
-//           console.log(`Nenhum jogador correspondente encontrado para "${jogador}".`);
-//           return(`Nenhum jogador correspondente encontrado para "${jogador}".`);
-//       }
-//   }).catch(error => {
-//       // Retorna erro
-//       console.log(error);
-//       return(error);
-//   });
-// }
-
-// getPlayerInfo('Cristiano Ronaldo')
