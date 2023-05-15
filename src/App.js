5ab41260bf734d42356f658f3cca84a58514f96a
Luan Ferreira <108829048+fluanbrito@users.noreply.github.com>
2023-05-15 13:32:43 -0300

    feat: Ajuste para apagar pesquisa anterior

--- src/App.js
+++ src/App.js
@@ -7,21 +7,32 @@ import GetDateInfo from './components/dateInfo';
 const App = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedDate, setSelectedDate] = useState(null);
+  const [showPlayerInfo, setShowPlayerInfo] = useState(false);
+  const [showTeamInfo, setShowTeamInfo] = useState(false);
 
   const handleSearch = query => {
     setSearchQuery(query);
+    setShowPlayerInfo(true);
+    setShowTeamInfo(true);
   };
 
   const handleDateChange = date => {
     setSelectedDate(date);
   };
 
+  const clearSearch = () => {
+    setSearchQuery('');
+    setSelectedDate(null);
+    setShowPlayerInfo(false);
+    setShowTeamInfo(false);
+  };
+
   return (
     <div>
       <h1>Aplicação de estatísticas esportivas</h1>
-      <SearchBar onSearch={handleSearch} />
-      <GetPlayerInfo searchQuery={searchQuery} />
-      <GetTeamInfo searchQuery={searchQuery} />
+      <SearchBar onSearch={handleSearch} onClear={clearSearch} />
+      {showPlayerInfo && <GetPlayerInfo searchQuery={searchQuery} />}
+      {showTeamInfo && <GetTeamInfo searchQuery={searchQuery} />}
       <GetDateInfo
         searchQuery={searchQuery}
         dataInicial={selectedDate}
